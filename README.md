468957e4f087b460cfe4998f49a14c91e0886c4e
Salmon Dehkanov <salmon@efir.uz>
2022-11-05 18:25:13 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# VoIP Blazer balance checker
+
+## Requirements
+
+1. Python Virtual Environment
+
+    python3 -m venv venv
+    source venv/bin/activate
+    pip install selenium pyTelegramBotAPI
+
+2. Chromedriver
+
+    https://chromedriver.chromium.org/downloads
+
+3. Cron Job
+
+4. Telegram Bot Token
+
+5. VoIP Blazer login/password
+
+
---
+++ Voipblazer.py
@@ -0,0 +1,45 @@
+import os
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options 
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import telebot
+import re
+
+PATH = "CHROME_DRIVER_PATH"
+
+options = Options()
+options.headless = True
+driver = webdriver.Chrome(os.getenv(PATH), options=options)
+
+driver.get("https://www.voipblazer.com/buy_credit2/")
+elements = driver.find_elements(By.CLASS_NAME, 'myloginform')
+
+login = None
+
+for element in elements:
+    block_style = element.get_attribute("style")
+    if block_style == "display: block;":
+        login = element
+        break
+    
+if login is None:
+    print("Could not find the login form, exiting...")
+    exit(0)
+
+login.find_element(By.XPATH, "div[2]/form/table/tbody/tr[1]/td[2]/input").send_keys("thanks84")
+login.find_element(By.XPATH, "div[2]/form/table/tbody/tr[2]/td[2]/input").send_keys("sister84")
+login.find_element(By.XPATH, 'div[2]/form/table/tbody/tr[5]/td[2]/input').send_keys(Keys.ENTER)
+
+balance = driver.find_element(By.CLASS_NAME, "balance").text
+print("The current balance: " + balance)
+
+b = float(balance.replace("€ ", "").replace(",", ""))
+if b < 700:
+    TOKEN = "5718569127:AAH-38l5rJl-JsEcSVd1YlEeBpQprkFqndY"
+    bot = telebot.TeleBot(TOKEN) 
+    bot.send_message(5494804052, 'ALERT: BALANCE IS LOW: ' + balance)
+
+
+
+
