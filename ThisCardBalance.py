9b704d20230d961d6064c69c11ce3c3a8511b856
Shuhrat Dehkanov <k@efir.uz>
2023-03-12 16:40:49 +0900

    THiS Card: set min threshold to 500K

--- ThisCardBalance.py
+++ ThisCardBalance.py
@@ -1,14 +1,16 @@
 import os
 import time
 import telebot
+
 from dotenv import dotenv_values
+
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+
 
 env_conf = dotenv_values(os.path.abspath(os.path.dirname(__file__)) + "/.env")
 
@@ -23,9 +25,10 @@ driver.find_element(
 driver.find_element(
     By.XPATH, '//*[@id="scroller"]/form/table/tbody/tr[8]/td/input').send_keys(env_conf.get("THIS_PASS"))
 time.sleep(5)
-login = driver.find_element(By.XPATH, '//*[@id="scroller"]/form/table/tbody/tr[11]/td/table/tbody/tr/td[1]/img')
+login = driver.find_element(
+    By.XPATH, '//*[@id="scroller"]/form/table/tbody/tr[11]/td/table/tbody/tr/td[1]/img')
 if login is None:
-    print("THISCARD: could not find the login form, exiting...")
+    print("THiS Card: could not find the login form, exiting...")
     exit(0)
 
 elem = WebDriverWait(driver, 10).until(EC.element_to_be_clickable(
@@ -34,15 +37,13 @@ elem.click()
 
 time.sleep(5)
 
-balance = driver.find_element(By.XPATH, '//*[@id="scroller"]/table/tbody/tr[3]/td/span')
+balance = driver.find_element(
+    By.XPATH, '//*[@id="scroller"]/table/tbody/tr[3]/td/span')
 balance = balance.get_attribute('textContent')
-print(balance)
+print("THiS Card Balance: ￦{0}".format(balance))
 
-
-print("THISCARD Balance: ￦{0}".format(balance))
-
-if float(balance.replace(",", "")) <= 1000000:
-    alert = "<b>ALERT!</b>\nThisCard balance is low: ￦{0}".format(balance)
+if float(balance.replace(",", "")) <= 500000:
+    alert = "<b>ALERT!</b>\nCK (디스카드) balance is low: ￦{0}".format(balance)
     bot = telebot.TeleBot(env_conf.get("TELEGRAM_BOT_TOKEN"))
     bot.send_message(chat_id=env_conf.get("TELEGRAM_CHAT_ID"),
                      parse_mode="HTML",
