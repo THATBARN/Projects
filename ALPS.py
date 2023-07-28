703f20f9689ff7e55dee804c6c4ab0c785c573da
Salmon Dehkanov <salmon@efir.uz>
2023-07-28 17:50:29 +0900

    added ALPS order uploader

---
+++ ALPS.py
@@ -0,0 +1,112 @@
+import requests
+from datetime import datetime
+from dotenv import dotenv_values
+import os
+env_conf = dotenv_values(os.path.abspath(os.path.dirname(__file__)) + "/.env")
+from openpyxl import load_workbook
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+# # GET new order ids
+url = "https://admin.bozoraka.com/api/new-market-request-ids?sellerId=25"
+headers = {'Authorization': 'Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJtYXJrZXQub3JkZXIuZXhwb3J0ZXIiLCJhdXRoIjoiUk9MRV9NQVJLRVRfTUFOQUdFUiIsImV4cCI6MTY5Mjc2MjM5MH0.xjnH9Y5idIhJplQATXMe5PAwp7ZlyAmCbYhwO7J246GfS7hSvuEw6Mdn-_owrLY2vBexWBRJhRxh0fqa8i4vJw'}
+response = requests.get(url, headers=headers)
+order_ids = response.text
+
+# # POST order ids to receive order excel file
+url = "https://admin.bozoraka.com/api/new-market-requests-excel"
+headers = {'Content-type': 'application/json', 'Authorization': 'Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJtYXJrZXQub3JkZXIuZXhwb3J0ZXIiLCJhdXRoIjoiUk9MRV9NQVJLRVRfTUFOQUdFUiIsImV4cCI6MTY5Mjc2MjM5MH0.xjnH9Y5idIhJplQATXMe5PAwp7ZlyAmCbYhwO7J246GfS7hSvuEw6Mdn-_owrLY2vBexWBRJhRxh0fqa8i4vJw'}
+response = requests.post(url, data=order_ids, headers=headers)
+
+temp_file = env_conf.get("ORDER_FOLDER_PATH") + "/temp.xlsx"
+with open(temp_file, 'wb') as e:
+    e.write(response.content) # creates temporary file of order excel spreadsheet
+
+# # Edit excel spreadsheet to remove duplicates...
+wb = load_workbook(filename=temp_file)
+sheet = wb.active
+
+os.remove(env_conf.get("ORDER_FOLDER_PATH") + "/temp.xlsx") # delete temp file, as it is now unneeded
+
+# puts all the order numbers in list
+column = sheet["A"]
+order_numbers = []
+for order in column:
+    order = order.value
+    order_numbers.append(order)
+order_numbers.remove(None)
+
+# cycles through list to find dupes
+dupe_orders = []
+for order in order_numbers:
+    if order.count("-") > 2: # finds order numbers with more than 2 dashes...
+        dupe_orders.append(order)
+        try:
+            dupe_orders.append(order[0:-2]) # ...then finds corresponding order
+        except ValueError:
+            continue # if not corresponding order, then ignore and continue (if there are three or more packages in one order)
+
+# deletes dupe rows
+rows = list(sheet.rows)
+for i in range(len(rows)):
+    if rows[i][0].value in dupe_orders:
+        sheet.delete_rows(i)
+
+
+now = datetime.now()
+dt_string = "/" + now.strftime("%Y%m%d-%H%M") + ".xlsx" # datetime object containing current date and time
+wb.save(env_conf.get("ORDER_FOLDER_PATH") + dt_string)
+
+
+
+# # Upload file to ALPS...
+options = Options()
+options.binary_location = "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"
+options.headless = True
+
+driver = webdriver.Chrome(env_conf.get("CHROME_DRIVER_PATH"), options=options)
+
+driver.get(env_conf.get("ALPS_URL"))
+driver.find_element(
+    By.XPATH, '//*[@id="principal"]/input').send_keys(env_conf.get("ALPS_ID"))
+driver.find_element(
+    By.XPATH, '//*[@id="credential"]/input').send_keys(env_conf.get("ALPS_PASS"))
+
+login = driver.find_element(By.XPATH, '//*[@id="btn-login"]')
+login.click()
+
+time.sleep(3)
+
+button = driver.find_element(
+    By.XPATH, '/html/body/div[3]/header/nav/div[1]/div[1]/i-button')
+button.click()
+
+button = driver.find_element(
+    By.XPATH, '/html/body/div[3]/header/nav/div[3]/div/div[2]/div[7]/div[2]/ul/li/a')
+button.click()
+
+time.sleep(3)
+
+workframe = driver.find_element(By.ID, "workframe_10782")
+driver.switch_to.frame(workframe)
+driver.execute_script("arguments[0].removeAttribute('readonly')",
+                      WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="cboUsrFmat01"]/input[2]'))))
+
+typeinput = driver.find_element(By.XPATH, '//*[@id="cboUsrFmat01"]/input[2]')
+typeinput.send_keys(Keys.ARROW_DOWN, Keys.ARROW_DOWN, Keys.ARROW_DOWN, Keys.ENTER)
+
+file_upload = driver.find_element(
+    By.XPATH, '//*[@id="files"]'
+)
+
+file_upload.send_keys(env_conf.get("ORDER_FOLDER_PATH") + dt_string)
+
+time.sleep(5)
+
+
--- README.md
+++ README.md
@@ -15,3 +15,19 @@
 4. Telegram Bot Token and Chat ID.
 
 5. Credentials.
+
+# ALPS
+
+## Requirements
+
+1. Python Virtual Environment:
+
+        python3 -m venv venv
+        source venv/bin/activate
+        pip install -r requirements.txt
+
+2. [Chromedriver](https://chromedriver.chromium.org/downloads).
+
+3. Credentials.
+
+4. Order spreadsheet storage folder (see .env)
