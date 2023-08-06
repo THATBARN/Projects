2c2e5233a2c1c94ffaec1c2ad1863ef74779f5f1
Shuhrat Dehkanov <k@efir.uz>
2023-08-06 15:38:02 +0900

    Fix more typos

--- Alps.py
+++ Alps.py
@@ -1,21 +1,23 @@
+import os
 import requests
+import tempfile
+import time
+
 from datetime import datetime
 from dotenv import dotenv_values
-import os
-
 from openpyxl import load_workbook
-import time
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 
 
 env_conf = dotenv_values(os.path.abspath(os.path.dirname(__file__)) + "/.env")
+print("Get new market requests")
 
-# # GET new order ids
+# GET new order ids
 url = "https://admin.bozoraka.com/api/new-market-request-ids?sellerId=25"
 headers = {
     'Content-type': 'application/json',
@@ -24,56 +26,50 @@ headers = {
 response = requests.get(url, headers=headers)
 order_ids = response.text
 
-# # POST order ids to receive order excel file
+# POST order ids to receive order excel file
 url = "https://admin.bozoraka.com/api/new-market-requests-excel"
 response = requests.post(url, data=order_ids, headers=headers)
 
-temp_file = env_conf.get("ORDER_FOLDER_PATH") + "/temp.xlsx"
-with open(temp_file, 'wb') as e:
-    e.write(response.content) # creates temporary file of order excel spreadsheet
+new_requests = tempfile.TemporaryFile()
+new_requests.write(response.content)
 
-# # Edit excel spreadsheet to remove duplicates...
-wb = load_workbook(filename=temp_file)
+# Remove multi-box orders
+wb = load_workbook(filename=new_requests)
 sheet = wb.active
 
-os.remove(env_conf.get("ORDER_FOLDER_PATH") + "/temp.xlsx") # delete temp file, as it is now unneeded
-
-# puts all the order numbers in list
-column = sheet["A"]
+# Put order numbers in list
 order_numbers = []
-for order in column:
-    order = order.value
-    order_numbers.append(order)
+columnA = sheet["A"]
+for cell in columnA:
+    order_numbers.append(cell.value)
 order_numbers.remove(None)
 
-# cycles through list to find dupes
+# Cycle through list to find dupes
 dupe_orders = []
 for order in order_numbers:
-    if order.count("-") > 2: # finds order numbers with more than 2 dashes...
+    if order.count("-") > 2:  # find order numbers with more than 2 dashes...
         dupe_orders.append(order)
         try:
-            dupe_orders.append(order[0:-2]) # ...then finds corresponding order
-        except ValueError:
-            continue # if not corresponding order, then ignore and continue (if there are three or more packages in one order)
+            # ...then find corresponding order
+            dupe_orders.append(order[0:-2])
+        except e as ValueError:
+            # FIXME: this should never happen!
+            print("ALERT: Something wrong happened!", e)
+            exit(-1)
 
-# deletes dupe rows
+# Delete dupe rows
 rows = list(sheet.rows)
 for i in range(len(rows)):
     if rows[i][0].value in dupe_orders:
         sheet.delete_rows(i)
 
-
 now = datetime.now()
-dt_string = "/" + now.strftime("%Y%m%d-%H%M") + ".xlsx" # datetime object containing current date and time
-wb.save(env_conf.get("ORDER_FOLDER_PATH") + dt_string)
+orders_xlsx = now.strftime("%Y%m%d-%H%M") + ".xlsx"
+wb.save(env_conf.get("ORDER_FOLDER_PATH") + orders_xlsx)
 
-
-
-# # Upload file to ALPS...
+print("Uploading to ALPS website ...")
 options = Options()
-options.binary_location = "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"
 options.headless = True
-
 driver = webdriver.Chrome(env_conf.get("CHROME_DRIVER_PATH"), options=options)
 
 driver.get(env_conf.get("ALPS_URL"))
@@ -103,12 +99,12 @@ driver.execute_script("arguments[0].removeAttribute('readonly')",
                       WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="cboUsrFmat01"]/input[2]'))))
 
 typeinput = driver.find_element(By.XPATH, '//*[@id="cboUsrFmat01"]/input[2]')
-typeinput.send_keys(Keys.ARROW_DOWN, Keys.ARROW_DOWN, Keys.ARROW_DOWN, Keys.ENTER)
+typeinput.send_keys(Keys.ARROW_DOWN, Keys.ARROW_DOWN,
+                    Keys.ARROW_DOWN, Keys.ENTER)
 
 file_upload = driver.find_element(
     By.XPATH, '//*[@id="files"]'
 )
 
-file_upload.send_keys(env_conf.get("ORDER_FOLDER_PATH") + dt_string)
-
-time.sleep(5)
+file_upload.send_keys(env_conf.get("ORDER_FOLDER_PATH") + orders_xlsx)
+print("Done uploading new market requests")
